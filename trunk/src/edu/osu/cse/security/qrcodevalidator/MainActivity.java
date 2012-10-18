ef6aee492f985f064b621b9e7e973af7107a5504
Mario Carneiro <di.gama@gmail.com>
2012-10-18 14:05:33 -0400

    QR link stuff

--- trunk/src/edu/osu/cse/security/qrcodevalidator/MainActivity.java
+++ trunk/src/edu/osu/cse/security/qrcodevalidator/MainActivity.java
@@ -1,20 +1,50 @@
 package edu.osu.cse.security.qrcodevalidator;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends Activity {
+    public TextView text;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView)findViewById(R.id.text);
+        final Button button = (Button)findViewById(R.id.go);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(
+                    MainActivity.this);
+                integrator.initiateScan();
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri
+                    .parse(text.getText().toString()));
+                startActivity(browserIntent);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(
+            requestCode, resultCode, intent);
+        if (scanResult != null) {
+            text.setText(scanResult.getContents());
+        }
     }
 }
